c106117bd5fd0de403f6e7c48b757d1d360ad0f6
may arden <mayard3n@gmail.com>
2021-05-11 16:36:24 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here
+
+class Breakfast {
+    constructor(food, drink) {
+      this.food = food;
+      this.drink = drink;
+    }
+};
+
+let newBreakfast = new Breakfast(' ', 'juice')
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink; 
+    }
+};
+
+class Dinner {
+    constructor(salad, soup, entree, _dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = _dessert;
+    }
+};
